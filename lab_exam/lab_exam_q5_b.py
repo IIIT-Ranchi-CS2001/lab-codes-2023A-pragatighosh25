de0d1edca8c0c3d48caa32e9de3d49a88bd208ae
Pragati Ghosh <145133614+pragatighosh25@users.noreply.github.com>
2024-12-03 12:39:35 +0530

    lab_exam_q5_b.py

--- lab_exam/lab_exam_q5_b.py
+++ lab_exam/lab_exam_q5_b.py
@@ -3,4 +3,4 @@ import pandas as pd
 data = pd.read_csv("AQI_Data.csv")
 
 #display first 10 rows showing columns City, AQI and PM2.5 
-print(data[["CITY", "AQI", "PM2.5"]].head(10))
+print(data[["City", "AQI", "PM2.5"]].head(10))
